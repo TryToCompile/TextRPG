f94edb1e9307181a357d808c19b2bba16939945b
TryToCompile <adam1999@tut.by>
2022-07-27 15:32:30 +0300

    init

---
+++ TextRPG/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace textRPG
+{
+    public class Creature
+    {
+        public string? name = "Undefined";
+        public double hpbar = 100;
+        public double manapool = 100;
+        public double stamina = 100;
+        public string Name { get; set; }
+        public double Hpbar { get; set; }
+        public double Manapool { get; set; }
+        public double Stamina { get; set; }
+
+    }
+
+    public class Player : Creature
+    {
+
+    }
+}
