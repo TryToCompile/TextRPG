2e8ff6e4997f8fb3bdc1f82b4092cd11fd37ee44
TryToCompile <adam1999@tut.by>
2022-07-27 16:56:35 +0300

    feat: add some feats

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -1,14 +1,46 @@
 ﻿using System.Linq;
+using textRPG;
+using static System.Console;
+/* TODO: 
+ * 1) Hi, warrior, choose your name
+ * 2) Fight the rat
+ * 3) Add system, that allows palyer to type some commands, that will give him information about his stats, profile
+ */
+
+Player player = new Player();
+
+WriteLine("Warrior!\nChoose ur name:");
+player.Name = ReadLine(); // TODO: check for exceptions with numbers or spaces or so
+WriteLine($"We are greeting u, the future god - { player.Name}!\nChoose ur weapon -> 1 - assassin's blades (10 dmg); 2 - HAMMER OF THE PURE SOUL (100 dmg) ");
+if (ReadLine() == "1")
+{
+    player.WeaponAttack = 10;
+    player.CritDamageChance = 33; // TODO: add randomiser in
+} else if (ReadLine() == "2")
+{
+    player.WeaponAttack = 100;
+    player.CritDamageChance = 5; // TODO: add randomiser in
+}
+
+while (player.Hpbar > 0)
+{
+
+}
+
 
 namespace textRPG
 {
     public class Creature
     {
-        public string? name = "Undefined";
-        public double hpbar = 100;
-        public double manapool = 100;
-        public double stamina = 100;
-        public string Name { get; set; }
+        private string? name = "Undefined";
+        private double hpbar = 100;
+        private double manapool = 100;
+        private double stamina = 100;
+        private double armour = 10;
+        private double attackPower = 10;
+        public double AttackPower { get; set; }
+        public double Armor { get; set; }
+        public string? Name { get; set; }
         public double Hpbar { get; set; }
         public double Manapool { get; set; }
         public double Stamina { get; set; }
@@ -17,6 +49,20 @@ namespace textRPG
 
     public class Player : Creature
     {
+        private double weaponAttack;
+        private double attack = weaponAttack + AttackPower;
+        private double critDamageChance = 10;
+        public double CritDamageChance { get; set; }
+        public double Attack { get; set; }
+        public double WeaponAttack { get; set; }
+    }
+
+    
+    public class Rat : Creature
+    {
 
     }
+
+    
 }
+
